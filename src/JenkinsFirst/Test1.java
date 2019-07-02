895659f6ab7d89abd220832cc825db4475a9eff4
anmiraje <40157942+anmiraje@users.noreply.github.com>
2019-07-02 16:24:14 +0530

    changes made and pushed

--- src/JenkinsFirst/Test1.java
+++ src/JenkinsFirst/Test1.java
@@ -2,9 +2,11 @@ package JenkinsFirst;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
-		  System.out.println("Test1 test cases");
+		  System.out.println("Test1 test cases");  
+		  System.out.println("Test1 test cases 23");    
 	}
 
 }
