251174efd931feac4e21f690802426bc867d1e71
amiraje <50877038+amiraje@users.noreply.github.com>
2019-07-03 14:31:36 +0530

    Update Test3.java

--- Sample/src/Test3.java
+++ Sample/src/Test3.java
@@ -2,7 +2,7 @@
 public class Test3 {
 
 	public static void main(String[] args) {
-		System.out.println("Second class calling aniket ");
+		System.out.println("Second class calling aniket as ");
 		System.out.println("Second 12 class calling");
 
 	}
